18e03e93ca04774db4449e838cbd8fbe30f50664
Olund <henke.olund@gmail.com>
2015-02-24 21:28:27 +0100

    Second try...

--- js/main.js
+++ js/main.js
@@ -1,23 +1,32 @@
 $(document).ready(function () {
     $('#fullpage').fullpage({
-        navigation: true,
-        navigationPosition: 'right',
-        navigationTooltips: ['Home', 'Extra', 'Extra2', 'Extra3'],
+        //navigation: true,
+       // navigationPosition: 'left',
+        anchors:['home', 'extra', 'extra2', 'extra3'],
+        menu: '#menu',
+        //navigationTooltips: ['Home', 'Extra', 'Extra2', 'Extra3'],
 
         //Scrolling
         css3: true,
-        scrollingSpeed: 700,
+        scrollingSpeed: 550,
         autoScrolling: true,
         scrollBar: false,
         easing: 'easeInQuart',
         easingcss3: 'ease',
 
         sectionsColor: [
-            '#1976D2',
-            '#4CAF50',
-            '#C2185B',
-            'whitesmoke',
-            '#000'
+            '#03A9F4',
+            '#FF5252',
+            '#1BBC9B',
+            '#FF5722',
         ],
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction){}
     });
 });
