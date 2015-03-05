38fa87f390b2b6eed334036c542484c8ec5ed604
Olund <henke.olund@gmail.com>
2015-03-05 18:15:31 +0100

    done for today

--- js/main.js
+++ js/main.js
@@ -13,8 +13,20 @@ $(document).ready(function () {
         belowOrigin: true
     });
 
+    // Date contact
     $('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 15 // Creates a dropdown of 15 years to control year
     });
+
+    // Chat modal
+    $('.modal-trigger').leanModal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .5, // Opacity of modal background
+        in_duration: 300, // Transition in duration
+        out_duration: 200, // Transition out duration
+        ready: function() { /*alert('Ready');*/ }, // Callback for Modal open
+        complete: function() { /*alert('Closed');*/ } // Callback for Modal close
+    });
+
 });
